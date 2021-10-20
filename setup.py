22d18d8d90ca728617301ff718e76faeb9d63148
in4maniac <sahanbull@gmail.com>
2021-10-20 17:38:24 +0100

    feat: pandas in the library

--- setup.py
+++ setup.py
@@ -10,6 +10,5 @@ setup(
     author_email='m.bulathwela@ucl.ac.uk',
     description='This repository contains the metadata dataset extracted from X5GON repository with helper code to load the dataset into Python environments for data processing.',
     install_requires=[
-        'numpy>=1.14.1',
-        'pandas>=1.3.3']
+        'pandas']
 )
